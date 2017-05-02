e9f2f50f2a019ac160abb96a92c840cf09d1441b
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-03 00:37:05 +0200

    Add tests results and uploaded files to submit details endpoint

--- src/Controllers/Submit.hs
+++ src/Controllers/Submit.hs
@@ -34,6 +34,34 @@ instance HasController (CurrentUser -> Key Contest -> Key ContestProblem -> Key 
             where_ $ submits ^. SubmitAuthor ==. val (entityKey user)
             return submits
 
+instance HasController (CurrentUser -> Key Contest -> Key ContestProblem -> Key Submit -> HandlerT IO (Entity' Submit SubmitWithFilesAndTests)) where
+  resourceController user _ _ submitId = do
+    res <- runQuery $ select $ from $ \(submits `LeftOuterJoin` testResults `LeftOuterJoin` files) -> do
+            on      $ submits ^. SubmitId ==. files ^. SubmitFileSubmit
+            on      $ submits ^. SubmitId ==. testResults ^. TestResultSubmit
+            where_  $ submits ^. SubmitId ==. val submitId
+            where_  $ submits ^. SubmitAuthor ==. val (entityKey user)
+            return (submits, files, testResults)
+
+    let files = collectionJoin $ map (\(submit, file, _) -> (submit, file)) res
+    let tests = collectionJoin $ map (\(submit, _, test) -> (submit, test)) res
+    let r = [(submit, file, test) | (submit, file) <- files, (submit, test) <- tests]
+    (es, ef, et) <- safeHead ErrNotFound $ r
+
+    return $ Entity' (entityKey es)
+            $ SubmitWithFilesAndTests
+            $ rAdd (Var :: Var "files") (fmap truncateFile ef)
+            $ rAdd (Var :: Var "tests") (fmap truncateTest et)
+            $ explode (entityVal es)
+    where truncateFile file = Entity' (entityKey file)
+            $ rDel (Var :: Var "file")
+            $ rDel (Var :: Var "submit")
+            $ explode $ entityVal file
+          truncateTest test = Entity' (entityKey test)
+            $ rDel (Var :: Var "submit")
+            $ explode $ entityVal test
+
+
 instance HasController (CurrentUser -> Key Contest -> Key ContestProblem -> MultipartData -> HandlerT IO (Entity' Submit SubmitWithFiles)) where
   resourceController user _ problem formData = do
     currentTimestamp <- liftIO T.getCurrentTime
